9c09c8c658e71a21a585ae2638aa1724ff64d1a1
jdbricker@gmail.com <jdbricker@gmail.com>
2026-08-18 14:20:04 +0000

    added comments

--- fetch_data.py
+++ fetch_data.py
@@ -5,10 +5,13 @@ STATIONS = [
     'USC00201492',
 ]
 
+#define TIMESTAMP here so it's the same for every file pulled in a given run
 TIMESTAMP = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-for station in STATIONS:
-    response = requests.get(F'https://www.ncei.noaa.gov/pub/data/ghcn/daily/hcn/{station}.dly')
-    
-    with open(f'/Volumes/data/default/data/weather/{station}_{TIMESTAMP}.txt', 'wb') as f:
-        f.write(response.content)
+if '__name__' == '__main__':
+
+    for station in STATIONS:
+        response = requests.get(F'https://www.ncei.noaa.gov/pub/data/ghcn/daily/hcn/{station}.dly')
+        
+        with open(f'/Volumes/data/default/data/weather/{station}_{TIMESTAMP}.txt', 'wb') as f:
+            f.write(response.content)
