b621f5d14fb5349d98e99a3888f51aa8acc806c2
jdbricker@gmail.com <jdbricker@gmail.com>
2026-08-20 16:55:47 +0000

    fix to get DAB deployed
    change some dependencies, change to serverless,

---
+++ src/fetch_data_notebook.py
@@ -0,0 +1,20 @@
+# Databricks notebook source
+# DBTITLE 1,Fetch weather data from NOAA
+import requests
+from datetime import datetime
+
+STATIONS = [
+    'USC00201492',
+]
+
+#define TIMESTAMP here so it's the same for every file pulled in a given run
+TIMESTAMP = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+for station in STATIONS:
+    response = requests.get(f'https://www.ncei.noaa.gov/pub/data/ghcn/daily/hcn/{station}.dly')
+    
+    output_file_name = f'/Volumes/data/default/data/weather/{station}_{TIMESTAMP}.txt'
+    print(output_file_name)
+
+    with open(output_file_name, 'wb') as f:
+        f.write(response.content)
